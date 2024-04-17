dd33e43ec83306188345199a1d1b7d72aebd110e
rachelfitzz <149213055+rachelfitzz@users.noreply.github.com>
2024-04-16 19:02:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// $(".banner").on("mouseover", function() {
-//     $(this).animate({height: +=10%});
-// });
-// $(".banner").on("mouseout", function() {
-//     $(this).animate({height: '-=20px'},500);
-// });
-
 $(document).ready(function() {
     $('.banner').hover(function(){
         this.classList.add('bannerLarge');
